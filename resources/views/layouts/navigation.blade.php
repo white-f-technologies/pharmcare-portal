8bf1019fae60e929ccf24f677425289c5abd4685
muhindo felex <whitefdj@gmail.com>
2026-08-13 00:30:03 +0300

    Security: Hide Vendor Control Center link from client desktop navigation menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -157,10 +157,12 @@
                                 <span>{{ __('System Diagnostics') }}</span>
                             </x-dropdown-link>
 
-                            <x-dropdown-link :href="route('vendor.dashboard')" class="flex items-center gap-2">
-                                <svg class="w-4 h-4 text-amber-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/></svg>
-                                <span>{{ __('Vendor Control Center') }}</span>
-                            </x-dropdown-link>
+                            @if(file_exists(storage_path('keys/private.key')))
+                                <x-dropdown-link :href="route('vendor.dashboard')" class="flex items-center gap-2">
+                                    <svg class="w-4 h-4 text-amber-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/></svg>
+                                    <span>{{ __('Vendor Control Center') }}</span>
+                                </x-dropdown-link>
+                            @endif
                         @endif
 
                         <!-- Authentication -->
